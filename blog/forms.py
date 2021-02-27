ef3312d3817dd9b74f4ef36eb6c3bbd33d4e757b
WojtK <wojtek.kucharekos@gmail.com>
2021-02-28 00:35:37 +0100

    little changes

--- blog/forms.py
+++ blog/forms.py
@@ -3,11 +3,11 @@ from .models import Comment
 
 
 class EmailPostForm(forms.Form):
-    name = forms.CharField(max_length=25)
+    imie = forms.CharField(max_length=25)
     email = forms.EmailField()
-    to = forms.EmailField()
-    comments = forms.CharField(required=False,
-                               widget=forms.Textarea)
+    do = forms.EmailField()
+    komentarz = forms.CharField(required=False,
+                                widget=forms.Textarea)
 
 
 class CommentForm(forms.ModelForm):
