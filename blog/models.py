57a9ec931bee2f0194e3f349ad238be0a7aaf742
WojtK <wojtek.kucharekos@gmail.com>
2021-02-28 01:40:28 +0100

    little changes

--- blog/models.py
+++ blog/models.py
@@ -52,9 +52,9 @@ class Comment(models.Model):
     post = models.ForeignKey(Post,
                              on_delete=models.CASCADE,
                              related_name='comments')
-    name = models.CharField(max_length=80)
+    name = models.CharField("imie",max_length=80)
     email = models.EmailField()
-    body = models.TextField()
+    body = models.TextField("komentarz")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
